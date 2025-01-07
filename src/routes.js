9bd828b0615c93ea3ddcd0b4523702aa18a6d25f
thiagowaib <thiagowaib@gmail.com>
2025-01-07 13:52:49 -0300

    Ping Controller

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const routes = new express.Router();
 const { authClient, authAdmin } = require('./middlewares/AuthAdmin');
 const { saveLead, getLeads } = require('./controllers/LeadController');
+const {ping} = require('./controllers/ConnectionController');
+
+routes.get('/ping', authClient, ping);
 
 routes.post('/lead', authClient, saveLead);
 routes.get('/lead', authAdmin, getLeads);
