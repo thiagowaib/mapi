88ce0b7ee55e97a4eb900d05a49fecba55df85b9
thiagowaib <thiagowaib@gmail.com>
2025-02-04 14:26:52 -0300

    [Hotfix] Telegram Data Masking

--- src/services/TelegramHelper.js
+++ src/services/TelegramHelper.js
@@ -1,25 +1,16 @@
 import fetch from 'node-fetch';
 
-const TelegramConfig = {
-    Bot: {
-        id: "7936973625:AAGA8B7Fkl9EVgvbRYQHh8kQAk93IaNsRL0"
-    },
-    Chats: {
-        consultoriaEleva: "-4729831328"
-    }
-}
-
 const sendMessageEnrollment = async (content) => {
     let message = "Nova aplicação para a Consultoria Eleva!\n\n";
     content.forEach(item => {
         message += `>> ${item.question}\n`;
         message += `- ${item.answer}\n\n`;
     });
-    await sendMessage(message.trim(), TelegramConfig.Chats.consultoriaEleva); 
+    await sendMessage(message.trim(), process.env.TELEGRAM_CHAT_ENROLLMENT); 
 }
 
 const sendMessage = async (message, chat) => {
-    const botToken = TelegramConfig.Bot.id;
+    const botToken = process.env.TELEGRAM_BOT;
     const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
     const response = await fetch(url, {
