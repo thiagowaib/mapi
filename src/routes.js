699d77bcd17b0295b902b7d676d5c245783036d9
thiagowaib <thiagowaib@gmail.com>
2025-05-19 21:18:12 -0300

    Confirmation Controller

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = new express.Router();
 const { authClient, authAdmin } = require('./middlewares/AuthAdmin');
 const { saveLead, getLeads } = require('./controllers/LeadController');
 const { saveEnrollment, getEnrollments } = require('./controllers/EnrollmentController')
+const { saveConfirmation, getConfirmations } = require('./controllers/ConfirmationController')
 const {ping} = require('./controllers/ConnectionController');
 
 routes.get('/ping', authClient, ping);
@@ -12,5 +13,8 @@ routes.get('/lead', authAdmin, getLeads);
 
 routes.post('/enrollment', authClient, saveEnrollment);
 routes.get('/enrollment', authAdmin, getEnrollments);
+
+routes.post('/confirmation', authClient, saveConfirmation);
+routes.get('/confirmation', authAdmin, getConfirmations);
 
 module.exports = routes
